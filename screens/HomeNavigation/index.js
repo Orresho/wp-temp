d2aa58386f482b0532bf06401f0cfc0bec09c7ed
Orhan Hajderovic <orhanhajderovic@Orhans-MacBook-Pro.local>
2019-03-08 14:09:21 +0100

    Move navigation + add default value to data argument

--- screens/HomeNavigation/index.js
+++ screens/HomeNavigation/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import NavigationList from '../../components/HomeNavigation/NavigationList';
 
-import navigationData from '../../components/HomeNavigation/_NavigationConfig';
+import navigationData from './_NavigationConfig';
 
 export default class HomeScreen extends Component {
 	static navigationOptions = {
@@ -9,7 +9,7 @@ export default class HomeScreen extends Component {
 	};
 
 	// Create press handler for each screen specified in the config file
-	_handleNavigate = (screen, data) => {
+	_handleNavigate = (screen, data=[]) => {
 		const { navigate } = this.props.navigation;
 		return navigate(screen, data)
 	}
